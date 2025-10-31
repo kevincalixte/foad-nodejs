7b3ec49e6c25346b6148b180dec8f7fdfb533fe8
kevincalixte <kevin.calixte@icloud.com>
2025-10-31 13:37:48 +0100

    fix fetch JSON & paths

--- client/assets/js/lib/card.js
+++ client/assets/js/lib/card.js
@@ -6,55 +6,60 @@ const createCard = () => {
     document.body.appendChild(cardContainer);
 
     fetch("http://localhost:8080/api/users")
-      .catch((error) => {
-        return fetch("./assets/data/users.json");
-      })
       .then((res) => res.json())
       .then((users) => {
-        users.forEach((user) => {
-          // CREATION DES ELEMENTS
-          const card = document.createElement("div");
-          const cardPrenom = document.createElement("p");
-          const cardNom = document.createElement("p");
-          const cardEmail = document.createElement("p");
-          const cardAge = document.createElement("p");
-          const cardAdresse = document.createElement("p");
-          const cardAvatar = document.createElement("img");
-          //  ATTRIBUTION DES IDs
-          card.id = "card";
-          cardPrenom.id = "cardPrenom";
-          cardNom.id = "cardNom";
-          cardEmail.id = "cardEmail";
-          cardAge.id = "cardAge";
-          cardAdresse.id = "cardAdresse";
-          cardAvatar.id = "cardAvatar";
-          // AJOUT DU CONTENU
-          cardPrenom.textContent = user.prénom;
-          cardNom.textContent = user.nom;
-          cardEmail.textContent = user.email;
-          cardAge.textContent = user.age + " ans";
-          cardAdresse.textContent =
-            user.adresse.rue +
-            ", " +
-            user.adresse.ville +
-            ", " +
-            user.adresse.pays;
-          cardAvatar.src = `./assets/img/${user.prénom.toLowerCase()}.svg`;
-          // AFFICHAGE DES ELEMENTS
-          cardContainer.append(card);
-          card.append(
-            cardPrenom,
-            cardNom,
-            cardEmail,
-            cardAge,
-            cardAdresse,
-            cardAvatar
-          );
-        });
+        if (users.length === 0) {
+          fetch("./assets/data/users.json")
+            .then((res) => res.json())
+            .then((users) => {
+              console.log(users);
+              pushJSON(users);
+            });
+        } else {
+          pushJSON(users);
+        }
       });
   } catch (error) {
     console.log("FETCH PROBLEM : " + error);
   }
 };
 
+function pushJSON(users) {
+  users.forEach((user) => {
+    // CREATION DES ELEMENTS
+    const card = document.createElement("div");
+    const cardPrenom = document.createElement("p");
+    const cardNom = document.createElement("p");
+    const cardEmail = document.createElement("p");
+    const cardAge = document.createElement("p");
+    const cardAdresse = document.createElement("p");
+    const cardAvatar = document.createElement("img");
+    //  ATTRIBUTION DES IDs
+    card.id = "card";
+    cardPrenom.id = "cardPrenom";
+    cardNom.id = "cardNom";
+    cardEmail.id = "cardEmail";
+    cardAge.id = "cardAge";
+    cardAdresse.id = "cardAdresse";
+    cardAvatar.id = "cardAvatar";
+    // AJOUT DU CONTENU
+    cardPrenom.textContent = user.prénom;
+    cardNom.textContent = user.nom;
+    cardEmail.textContent = user.email;
+    cardAge.textContent = user.age + " ans";
+    cardAdresse.textContent =
+      user.adresse.rue + ", " + user.adresse.ville + ", " + user.adresse.pays;
+    cardAvatar.src = `./assets/img/${user.prénom.toLowerCase()}.svg`;
+    // AFFICHAGE DES ELEMENTS
+    cardContainer.append(card);
+    card.append(
+      cardPrenom,
+      cardNom,
+      cardEmail,
+      cardAge,
+      cardAdresse,
+      cardAvatar
+    );
+  });
+}
 export { createCard };
--- server/index.js
+++ server/index.js
@@ -7,21 +7,22 @@ const app = express();
 app.use(express.json());
 
 // Configuration CORS pour permettre les requêtes depuis n'importe quelle origine
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   
-  // Répondre aux requêtes OPTIONS (preflight)
-  if (req.method === 'OPTIONS') {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-});
+//   // Répondre aux requêtes OPTIONS (preflight)
+//   if (req.method === 'OPTIONS') {
+//     res.sendStatus(200);
+//   } else {
+//     next();
+//   }
+// });
 
-app.use(express.static(path.join(__dirname, '../client')));
-app.use(express.static(path.join(__dirname, '../')));
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, './client/index.html'));
+// });
 
 mongoose
   .connect("mongodb://mongodb-service:27017/users")
@@ -29,6 +30,11 @@ mongoose
   .catch((err) => console.log("Connection error : ", err));
 
 app.use("/api/users", usersRoutes);
+console.log("chemin __dirname ==> " + __dirname);
+
+app.use(express.static(path.join(__dirname, '../client')));
+
+// Route pour la page d'accueil
 
 app.listen(portServer, "0.0.0.0", () => {
   console.log(`Server at port ${portServer}`);
